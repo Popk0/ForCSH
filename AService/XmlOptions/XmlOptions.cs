8b373d7a0b2e8b1aa64361dd794175d0ad8b34c7
Ilya <65541361+Popk0@users.noreply.github.com>
2020-12-14 02:51:08 +0300

    Add files via upload

---
+++ AService/XmlOptions/XmlOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace AService.XmlOptions
+{
+    class Options
+    {
+        public string SourcePath { get; set; }
+        public string TargetPath { get; set; }        
+        public string Command { get; set; }        
+        public Options(string sourcePath, string targetPath, string command)
+        {
+            SourcePath = sourcePath;
+            TargetPath = targetPath;                    
+            Command = command;                    
+        }
+    }
+}
