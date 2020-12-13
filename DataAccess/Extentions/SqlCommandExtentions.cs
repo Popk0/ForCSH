1c96d70747a7e57f87c2ac815ad9d7c7f3bf6ef6
Ilya <65541361+Popk0@users.noreply.github.com>
2020-12-14 02:57:14 +0300

    Add files via upload

---
+++ DataAccess/Extentions/SqlCommandExtentions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace DataAccess.SqlCommandExtentions
+{
+    public static class SqlCommandExtentions
+	{
+		public static IEnumerable<TEntity> ReadMany<TEntity>(this SqlCommand command) where TEntity : new()
+		{
+			var result = ReadManyInternal<TEntity>(command);
+			return result;
+		}
+
+		private static IEnumerable<TEntity> ReadManyInternal<TEntity>(SqlCommand command) where TEntity : new()
+		{
+			var reader = command.ExecuteReader();
+
+			if (!reader.HasRows)
+			{
+				reader.Close();
+				return Enumerable.Empty<TEntity>();
+			}
+
+			var entities = reader.ParseFromReaderInternal<TEntity>();
+			reader.Close();
+
+			return entities;
+		}
+        private static IEnumerable<TEntity> ParseFromReaderInternal<TEntity>(this SqlDataReader reader) where TEntity : new()
+        {
+            var entityType = typeof(TEntity);
+            var entityProps = entityType.GetProperties();
+
+            var entities = new List<TEntity>();            
+
+            //object valueFromReader;
+
+            while (reader.Read())
+            {
+                var entity = new TEntity();
+                foreach (var entityPropInfo in entityProps)
+                {                    
+                    try
+                    {
+                        if (reader[entityPropInfo.Name] is DBNull)
+                        {
+                            entityPropInfo.SetValue(entity, null);
+                            continue;
+                        }
+                        var valueFromReader = reader[entityPropInfo.Name];
+                        entityPropInfo.SetValue(entity, valueFromReader);
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        continue;
+                    }                    
+                }
+                entities.Add(entity);
+            }
+            return entities;
+        }
+    }
+}
