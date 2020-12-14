563025b276c9fb3c1356a65da663491be066bdfc
Ilya <65541361+Popk0@users.noreply.github.com>
2020-12-14 03:04:01 +0300

    Add files via upload

---
+++ Models/Region.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Models
+{
+    public class Region
+    {        
+        public string RegionDescription { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Country { get; set; }
+    }
+}
+
+
+
+
+
