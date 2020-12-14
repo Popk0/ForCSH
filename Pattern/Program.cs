c77ab481b508fa2e53ddb3258134f1ba7f4fbbd9
Ilya <65541361+Popk0@users.noreply.github.com>
2020-12-14 03:08:05 +0300

    Add files via upload

---
+++ Pattern/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Data.SqlClient;
+using System.Configuration;
+using DataAccess;
+using Models.SearchModels;
+using System.Xml.Serialization;
+using Models;
+using System.Collections.Generic;
+
+namespace Pattern
+{
+    public class Program
+    {        
+        public static void Proc(string TargetPath, string fileName, string command)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["Northwind"];
+
+            var regionsRepository = new RegionsRepository(connectionString.ConnectionString);
+
+            var searchCriteria = new SearchCriteria
+            {
+                RegionID = 1,
+            };            
+
+            var result = regionsRepository.GetRegions(searchCriteria, command);                        
+
+            XmlSerializer formatter = new XmlSerializer(result.GetType());
+
+            using (FileStream fs = new FileStream(Path.Combine(TargetPath, fileName), FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, result);               
+            }
+           
+        }
+
+    }
+}
